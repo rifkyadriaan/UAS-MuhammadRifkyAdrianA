34fa9ce5a717b26ac8f0fa804b681ad5e53e571a
rifkyadriaan <rifkyadrian11@gmail.com>
2022-07-06 18:41:21 +0700

    UAS-MuhammadRifkyA
    Uas mobile programming

---
+++ app/src/main/java/com/example/Uas_MuhammadRifkyAdrian/CarsActivity.java
@@ -0,0 +1,29 @@
+package com.example.Uas_MuhammadRifkyAdrian;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uas_rifkyadrian.R;
+
+import java.util.ArrayList;
+
+public class CarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars);
+
+        ArrayList<CarsDetails> carsDetails = QueryUtils.extractCarsDetails();
+
+        ListView carsListView = (ListView) findViewById(R.id.list);
+
+        CarsAdapter adapter = new CarsAdapter(this, carsDetails);
+
+        carsListView.setAdapter(adapter);
+
+        // App successfully parsed JSON from QueryUtils class...
+    }
+}
